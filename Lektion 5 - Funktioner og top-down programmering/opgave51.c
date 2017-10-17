25fcfa49af3c1c7c9a7109f774fdff0dac9b06fb
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2017-10-17 15:53:31 +0200

    Lektion 5
    Not done.

---
+++ Lektion 5 - Funktioner og top-down programmering/opgave51.c
@@ -0,0 +1,14 @@
+/*
+Aflevering nr. 4
+Opgave 5.1
+--
+Sigvardsen_Tummas_20164825
+--
+Tummas Jóhan Sigvardsen,
+Studienr.: 20164825
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void solveQuadraticEquation(double a, double b, double c);
