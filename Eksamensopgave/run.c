55ae9d6a5bcf1e130a22ec1138571d42f1c5019f
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2017-11-23 22:28:10 +0100

    a
    a

--- Eksamensopgave/run.c
+++ Eksamensopgave/run.c
@@ -2,29 +2,88 @@
  * File:   main.c
  * Author: zentut.com
  * Description: Read text file line by line and output it to
- *			  the screen.
+ *            the screen.
  */
 
 /* http://www.zentut.com/c-tutorial/c-read-text-file/ */
  
+
+/*
 #include <stdio.h>
  
 #define MAXCHAR 110
 int main() {
-	FILE *fp;
-	char buffer[MAXCHAR];
-	char* filename = "cykelloeb-2017";
-	char* lineFormat = "%s\n";
-	fp = fopen(filename, "r");
-	if (fp == NULL){
-		printf("Could not open file %s",filename);
-		return 1;
-	}
+    FILE *fp;
+    char buffer[MAXCHAR];
+    char* filename = "cykelloeb-2017";
+    char* lineFormat = "%s\n";
 
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
 
-	while()
-	while (fgets(str, MAXCHAR, fp) != NULL)
-		printf("%s", str);
-	fclose(fp);
-	return 0;
+    while (fgets(str, MAXCHAR, fp) != NULL)
+        printf("%s", str);
+    fclose(fp);
+    return 0;
 }
+*/
+
+/*eNum*/
+/* Boolean sum default setur til FALSE, so um teir ikki eru DNF so bara FALSE. */
+
+typedef struct 
+{
+    *biker biker;
+    int positon;
+    time time;
+
+}event;
+
+typedef struct
+{
+    *nation nation;
+    int age;
+    *team team;
+
+}biker;
+
+
+typedef struct 
+{
+    *biker bikers;
+    char name[4];      
+}team;
+
+typedef struct 
+{
+   *biker bikers;
+    char name[4];  
+}nation;
+
+typedef struct 
+{
+   int hours;
+   int minutes;
+   int seconds;
+
+}time;
+
+
+typedef struct 
+{
+    *event results;
+    char* name;
+
+}race;
+
+
+
+
+
+
+
+// race.bikerRes.biker.nation
+// nation.time.bikers
