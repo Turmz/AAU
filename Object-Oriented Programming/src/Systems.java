727d5d9e28ef236056a88349a4a484eb34d2f4c9
Tummas Jóhan Sigvardsen <tummas@tummas.me>
2018-05-02 19:38:15 +0200

    Assignment 5 :: Started

---
+++ Object-Oriented Programming/src/Systems.java
@@ -0,0 +1,30 @@
+/*
+ *
+ * -------------------------------------------------------
+ * Name:             Tummas Jóhan Sigvardsen
+ * Email:            tsigva16@student.aau.dk
+ * Student number:   20164825
+ * Course:           Objektorienteret Programmering (OOP) (DAT2, SW2, IxD4, MAT-tilvalg) - AAL - F18
+ * Education:        Interaktionsdesign
+ * Semester:         4. semester
+ * -------------------------------------------------------
+ *
+ */
+
+//ASSIGNMENT
+//Systems.
+//        A system is a hexagonal region of space that contains planets and ships. A
+//        system has up to six neighboring systems. A system may have less than six neighbors if
+//        it is on the outer rim. We can identify the neighbors by their position: We have a system
+//        to the north, to the north-east, to the south-east, to the south, to the south-west, and
+//        nally to the north-west. A system has between zero and three planets. The planets in a
+//        system never change. Ships, on the other hand, may enter and leave a system. Figure 1
+//        shows an example of a Galaxy with systems and planets (ships not shown).
+//
+//        Problem 5. Write a class to represent a system. Add an appropriate constructor and
+//        methods for ships to enter and leave the system. Add a method to retrieve all the ships
+//        currently in the system.
+
+public class Systems {
+
+}
